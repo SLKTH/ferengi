f4071fd3ed0b1965b2282030a0bad7527a29a6d3
SLKTH <selektah@outlook.com>
2015-11-20 10:35:13 +0100

    Update ferengi.py
    Added an exception for rule '223' because of markup, and removed whitespace. Looks cleaner now.

--- ferengi.py
+++ ferengi.py
@@ -5,10 +5,14 @@
 import urllib
 import random
 from BeautifulSoup import *
+import sys
 
 # No copyright infrigment intended: thanks Stephen Jacob!
 # I generate this content from his website: www.sjtrek.com
 # Please show your support and take a look ;-)
+
+
+    
 
 # The URL
 url = 'http://www.sjtrek.com/trek/rules/'
@@ -25,14 +29,17 @@ tags = soup('li')
 # A dictionary for all the rules ('Oh silly me. But of course, it's a dictionary!')
 x = {}
 
-# Fill the dictionary with all the table contents (rule & rule number)
+# Fill the dictionary with all the table contents (rule (minus whitespace) & rule number).
 for il in tags:
-    x[il.get('value', None)] = str(il.contents[0])
+    x[il.get('value', None)] = str(il.contents[0].strip())
 
 # Pick a random dictionary item
-randomdict = random.choice(x.keys())  
+randomdictkey = random.choice(x.keys())  
+randomdictvalue = x[randomdictkey]
 
-# Output the magic.
-print "Rule Number " + randomdict + ":" + x[randomdict]
+# If the value is 223, then it contains an <i> tag and I'm making an exception: replace the value
+if randomdictkey == 223:
+    randomdictvalue = "Beware the man who doesn't make time for oo-mox."
     
-
+# Output the magic.
+print "Rule Number " + str(randomdictkey) + ": " + randomdictvalue
